83bcc8e803298d13ccf39333ac293203316318e5
Mihail2403 <mihailpickalev@gmail.com>
2023-10-01 21:25:03 +0500

    frontend:navbar.select_person.eat_item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">ТехновизорЕда</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" aria-current="page" to="/dcd">History</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
 
-nav {
-  padding: 30px;
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <select v-model="selected" @change.prevent="console.log(selected)">
+      <option v-for="person in persons" :key="persons.indexOf(person)">
+        {{person.name}}
+      </option>
+    </select>
+  <button @click.prevent="addToBasket">hbfh</button>
+  <div v-for="eat in basket" :key="eat.id">
+    <eat-item :eat-item="eat"/>
   </div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import EatItem from "@/components/eatItem.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: {EatItem},
+  data(){
+    return {
+      selected: '',
+      persons:[
+        {
+          id:0,
+          name:"Mic"
+        },
+        {
+          id:1,
+          name:"Nina"
+        }
+      ],
+      eats: [
+        {
+          id:0,
+          text:"hio",
+          components: "Chicken",
+          price: 12000
+        },
+        // {
+        //   id:1,
+        //   text:"hio",
+        //   components: "Chicken",
+        //   price: 12000
+        // }
+      ],
+      basket:[
+        {
+          id:0,
+          text:"hio",
+          components: "Chicken",
+          price: 12000
+        },
+      ]
+    }
+  },
+  methods: {
+    addToBasket(){
+      this.basket = [{text:"c", price:12}, ...this.basket]
+    }
   }
 }
 </script>
+
+<style scoped>
+
+</style>
