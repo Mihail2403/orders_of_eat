0bedffcd6146b825504d9af63732b53285cd29c7
Mihail2403 <mihailpickalev@gmail.com>
2023-10-04 19:36:13 +0500

    frontend:chatgeDate.submitForm

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-createApp(App).use(router).mount('#app')
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+createApp(App).use(router).component('VueDatePicker', VueDatePicker).mount('#app')
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,29 +3,41 @@
   <div class="container">
     <h4 class="row justify-content-center">Заказ</h4>
     <div class="row justify-content-center">
-      <select 
-        v-model="selectedPers" 
-        @change.prevent="console.log(selectedPers)"
-        class="form-select" 
+      <select
+        v-model="selectedPers"
+        @change.prevent="console.log(selectedPers);"
+        class="form-select"
         aria-label="Default select example"
         aria-placeholder="Select"
         style="
           width: 80%;
           margin-top: 4px;
-          
         "
       >
+
           <option
-            v-for="person in persons" 
+            v-for="person in persons"
             :key="persons.indexOf(person)"
-            @click.prevent=""
           >
             {{person.name}}
           </option>
         </select>
     </div>
     <div class="row justify-content-center">
-      <select 
+      <VueDatePicker
+        v-model="date"
+        :enable-time-picker="false"
+        :min-date="Date.now()"
+        :start-date="startDate"
+        ignore-time-validation
+        @update:model-value="console.log(this.date)"
+        style="
+          width: 82%;
+          margin-top: 4px;
+    "
+    />
+      <select
+          id="selectEats"
           v-model="selectedEat" 
           @change.prevent="addToBasket"
           class="form-select" 
@@ -37,22 +49,26 @@
           "
         >
             <option 
-              v-for="eatItem in eats" 
+              v-for="eatItem in eats"
+              :value="eatItem"
               :key="eats.indexOf(eatItem)"
             >
               {{eatItem.text}}
             </option>
           </select>
+      <div
+      v-for="eat in basket"
+      :key="eat.id"
+      >
+        <eat-item :eat-item="eat" @deleteFromBasket="deleteFromBasket"/>
       </div>
+      <button
+          class="btn"
+          style="margin-top: 4px"
+      >
+        Заказать</button>
+    </div>
   </div>
-
-  <div 
-    v-for="eat in basket" 
-    :key="eat.id"
-  >
-    <eat-item :eat-item="eat" @deleteFromBasket="deleteFromBasket"/>
-  </div>
-
 </template>
 
 <script>
@@ -62,16 +78,21 @@ export default {
   components: {EatItem},
   data(){
     return {
+      startDate: Date.now()+1,
+      date:null,
       keyForBasket:0,
       selectedEat:'',
       selectedPers: '',
       persons:[
         {
-          id:0,
-          name:"Mic"
+          name:"Выберите сотрудника"
         },
         {
           id:1,
+          name:"Mic"
+        },
+        {
+          id:2,
           name:"Nina"
         }
       ],
@@ -111,14 +132,18 @@ export default {
   methods: {
     addToBasket(){
       console.log(1)
+      if(this.basket.length > 10){
+        this.selectedEat=this.eats[0]
+        return
+      }
       var curr_eat = {
         id: this.keyForBasketCom(),
-        eat: this.eats.find(obj=>obj.text === this.selectedEat)
+        eat: this.eats.find(obj=>obj === this.selectedEat)
       }
       console.log(curr_eat)
       this.basket = [curr_eat, ...this.basket]
       console.log(this.basket)
-      this.selectedEat=this.eats[0].text
+      this.selectedEat=this.eats[0]
     },
     deleteFromBasket(eatItem){
       this.basket = this.basket.filter(eat => eat !== eatItem)
@@ -126,7 +151,11 @@ export default {
     keyForBasketCom() {
       this.keyForBasket += 1
       return this.keyForBasket
-    }
+    },
+  },
+  mounted() {
+    this.selectedPers = this.persons[0].name
+    this.selectedEat = this.eats[0]
   }
 }
 </script>
